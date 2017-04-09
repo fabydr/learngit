b116cccf74a7147dac353b619c2220c20588258a
Fabiola Robinson <faby@powerdata.com>
2017-04-09 15:09:44 -0700

    Checking into branch faby1

---
+++ label.java
@@ -0,0 +1,7 @@
+public class label
+{
+	static private void main(String args[])
+	{
+		System.out.println("Creating label");
+	}
+}
--- test.java
+++ test.java
@@ -1,5 +1,9 @@
 public class test
 {
+	public test()
+	{
+		int t = 0;
+	}
 	static private void main(String args[])
 	{
 		System.out.println("Hello World from Java");
